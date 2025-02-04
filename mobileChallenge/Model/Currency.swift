1b1416326ea705ef988346ccd61ed43eebfccd8a
henhiga <henrihiga@gmail.com>
2025-02-03 21:30:55 -0300

    Add API call

---
+++ mobileChallenge/Model/Currency.swift
@@ -0,0 +1,8 @@
+//
+//  Currency.swift
+//  mobileChallenge
+//
+//  Created by Henrique on 03/02/25.
+//
+
+import Foundation
---
+++ mobileChallenge/Model/CurrencyConversion.swift
@@ -0,0 +1,14 @@
+//
+//  CurrencyConversion.swift
+//  mobileChallenge
+//
+//  Created by Henrique on 03/02/25.
+//
+
+import Foundation
+
+struct Currency: Codable {
+    let currencies: [String: String]
+}
+
+
